c7fd7a20b8be1953c41df5e1e7ca656fd98dab97
cw4t <cw4t@qq.com>
2023-03-08 15:05:33 +0800

    修复PlayerLotteryLog的UID的unique属性，去掉，因为一个人会多次抽取
    Signed-off-by: cw4t <cw4t@qq.com>

--- models/player_log.go
+++ models/player_log.go
@@ -4,7 +4,7 @@ package models
 // 记录用户抽取的运势签
 type PlayerLotteryLog struct {
 	Model
-	Uid       string `gorm:"column:uid;type:varchar(191);unique" json:"uid"`     //用户ID
+	Uid       string `gorm:"column:uid;type:varchar(191)" json:"uid"`            //用户ID
 	Timestamp int    `gorm:"column:timestamp;type:tinyint(20)" json:"timestamp"` //事件发生时间
 	Score     int    `gorm:"column:score;type:tinyint(3)" json:"score"`          //运势分数
 	Keyword   string `gorm:"column:keyword;type:varchar(20)" json:"keyword"`     //运势关键字
